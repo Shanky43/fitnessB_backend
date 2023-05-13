c14ad150810c40b8e525634f957c8e94c977d85f
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-13 13:25:34 +0530

    added products

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -4,27 +4,41 @@ const ProductModel = require("../Model/products.model");
 const { auth } = require("../Middleware/auth.middleware");
 
 ProductRoute.get("/", async (req, res) => {
-    try {
-      const product = await ProductModel.find();
-      res.status(200).send({ products: product });
-    } catch (err) {
-      res.status(400).send({ err: err.message });
-    }
-  });
-  
-  ProductRoute.get("/:equipment", async (req, res) => {
-    let filters = { equipment: req.params.equipment };
-    console.log(req.query)
-    if (req.query.name) {
-      filters.name = { $regex: req.query.name, $options: "i" };
-    }
-    try {
-      let filteredProduct = await ProductModel.find(filters)
-      res.status(200).send({ products: filteredProduct });
-  
-    } catch (err) {
-      res.status(400).send({ err: err.message });
-    }
-  });
-   
-  module.exports = { ProductRoute };
+  try {
+    const product = await ProductModel.find();
+    res.status(200).send({ products: product });
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
+});
+
+ProductRoute.get("/:equipment", async (req, res) => {
+  let filters = { equipment: req.params.equipment };
+  console.log(req.query)
+  if (req.query.name) {
+    filters.name = { $regex: req.query.name, $options: "i" };
+  }
+  try {
+    let filteredProduct = await ProductModel.find(filters)
+    res.status(200).send({ products: filteredProduct });
+
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
+});
+
+ProductRoute.get("/:target", async (req, res) => {
+  let filters = { equipment: req.params.equipment };
+  console.log(req.query)
+  if (req.query.name) {
+    filters.name = { $regex: req.query.name, $options: "i" };
+  }
+  try {
+    let filteredProduct = await ProductModel.find(filters)
+    res.status(200).send({ products: filteredProduct });
+
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
+});
+module.exports = { ProductRoute };
