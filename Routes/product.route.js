3acd9a9d2f32fac037a5c6245e2829dbee891a53
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-13 14:38:02 +0530

    added products

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -8,6 +8,7 @@ ProductRoute.get("/", async (req, res) => {
     const product = await ProductModel.find();
     res.status(200).send({ products: product });
   } catch (err) {
+    console.log(err, "line11")
     res.status(400).send({ err: err.message });
   }
 });
@@ -23,11 +24,12 @@ ProductRoute.get("/:equipment", async (req, res) => {
     res.status(200).send(filteredProduct);
 
   } catch (err) {
+    console.log(err, "line27")
     res.status(400).send({ err: err.message });
   }
 });
 
-ProductRoute.get("/:equipment/:id", async (req, res) => {
+ProductRoute.get("/:equipments/:id", async (req, res) => {
   const { id } = req.params
   console.log(req.query)
   // if (req.query.name) {
@@ -38,30 +40,30 @@ ProductRoute.get("/:equipment/:id", async (req, res) => {
     res.status(200).send(filteredProduct);
 
   } catch (err) {
+    console.log(err, "line43")
     res.status(400).send({ err: err.message });
   }
 });
-ProductRoute.get("/exercise/:target", async (req, res) => {
+
+
+
+ProductRoute.get("/:exercise/:target", async (req, res) => {
   let filters = { target: req.params.target };
-  // console.log(req.query)
-  // if (req.query.name) {
-  //   filters.name = { $regex: req.query.name, $options: "i" };
-  // }
+
   try {
     let filteredProduct = await ProductModel.find(filters)
     res.status(200).send(filteredProduct);
 
   } catch (err) {
+    console.log(err, "line55")
     res.status(400).send({ err: err.message });
   }
 });
 
 ProductRoute.get("/exercise/:target/:id", async (req, res) => {
+
   const { id } = req.params
-  console.log(req.query)
-  // if (req.query.name) {
-  //   filters.name = { $regex: req.query.name, $options: "i" };
-  // }
+  console.log(id, "line66")
   try {
     let filteredProduct = await ProductModel.findById({ _id: id })
     res.status(200).send(filteredProduct);
